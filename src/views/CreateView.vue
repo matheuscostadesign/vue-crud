b94d0058fd75b659a98e674f45f3e3e0c744cc18
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-06-09 10:55:04 -0300

    Adicionado metodo POST

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,3 +1,34 @@
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      users: [],
+      firstName: "",
+      lastName: "",
+    };
+  },
+  methods: {
+    async createUser() {
+      try {
+        const user = await axios.post("https://62950af263b5d108c199071e.mockapi.io/Usuarios", {
+          firstName: this.firstName,
+          lastName: this.lastName,
+        });
+        console.log(user);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="create">Cadastrar</div>
+  <form>
+    <input type="text" v-model="firstName" placeholder="Nome" required /> <br />
+    <input type="text" v-model="lastName" placeholder="Sobrenome" required /> <br />
+    <button @click="createUser">Cadastrar</button>
+  </form>
 </template>
--- src/views/ReadView.vue
+++ src/views/ReadView.vue
@@ -1,3 +1,53 @@
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    async getUsers() {
+      try {
+        const users = await axios.get("https://62950af263b5d108c199071e.mockapi.io/Usuarios");
+
+        this.users = users.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="home">Listar</div>
+  <button @click="getUsers">Mostrar</button> <br />
+  <br />
+
+  <table>
+    <tr>
+      <th>Nome</th>
+      <th>Sobrenome</th>
+      <th>Controles</th>
+    </tr>
+    <tr class="users" v-for="user in users" :key="user.id">
+      <td>{{ user.firstName }}</td>
+      <td>{{ user.lastName }}</td>
+      <td>
+        <button>Editar</button>
+        <button>Excluir</button>
+      </td>
+    </tr>
+  </table>
 </template>
+
+<style>
+table {
+  text-align: left;
+}
+th,
+td {
+  padding: 6px;
+}
+</style>
